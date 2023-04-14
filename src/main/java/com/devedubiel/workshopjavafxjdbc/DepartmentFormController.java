6fefa686e9ef6d119ce52aee87b2c1bde62ac76c
DevEduBiel <eduardo.vitorino@sptech.school>
2023-04-14 14:34:04 -0300

    DepartmentFormController

---
+++ src/main/java/com/devedubiel/workshopjavafxjdbc/DepartmentFormController.java
@@ -0,0 +1,46 @@
+package com.devedubiel.workshopjavafxjdbc;
+
+import com.devedubiel.workshopjavafxjdbc.controllers.util.Constraints;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class DepartmentFormController implements Initializable {
+
+    @FXML
+    private TextField txtId;
+    @FXML
+    private TextField txtName;
+    @FXML
+    private Label labelErroName;
+    @FXML
+    private Button btSave;
+    @FXML
+    private Button btCancel;
+
+    @FXML
+    public void obBtSaveAction(){
+        System.out.println("onBtSaveAction");
+    }
+
+    @FXML
+    public void obBtCancelAction(){
+        System.out.println("onBtCancelAction");
+    }
+
+    private void initializeNodes(){
+        Constraints.setTextFieldInteger(txtId);
+        Constraints.setTextFieldMaxLength(txtName,30);
+    }
+
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        initializeNodes();
+    }
+}

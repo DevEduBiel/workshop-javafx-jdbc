0a9ce110f227ea961a36e1d8b7274e31f7dff57a
DevEduBiel <eduardo.vitorino@sptech.school>
2023-04-14 14:56:49 -0300

    Salvando um novo Departamento

--- src/main/java/com/devedubiel/workshopjavafxjdbc/DepartmentFormController.java
+++ src/main/java/com/devedubiel/workshopjavafxjdbc/DepartmentFormController.java
@@ -1,9 +1,15 @@
 package com.devedubiel.workshopjavafxjdbc;
 
+import com.devedubiel.workshopjavafxjdbc.controllers.util.Alerts;
 import com.devedubiel.workshopjavafxjdbc.controllers.util.Constraints;
+import com.devedubiel.workshopjavafxjdbc.controllers.util.Utils;
+import com.devedubiel.workshopjavafxjdbc.database.DbException;
 import com.devedubiel.workshopjavafxjdbc.model.entities.Department;
+import com.devedubiel.workshopjavafxjdbc.services.DepartmentService;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
@@ -13,6 +19,7 @@ import java.util.ResourceBundle;
 
 public class DepartmentFormController implements Initializable {
     private Department entity;
+    private DepartmentService service;
 
     @FXML
     private TextField txtId;
@@ -29,14 +36,36 @@ public class DepartmentFormController implements Initializable {
         this.entity = entity;
     }
 
-    @FXML
-    public void obBtSaveAction(){
-        System.out.println("onBtSaveAction");
+    public void setService(DepartmentService service) {
+        this.service = service;
     }
 
     @FXML
-    public void obBtCancelAction(){
-        System.out.println("onBtCancelAction");
+    public void obBtSaveAction(ActionEvent event){
+        if (entity == null){
+            throw  new IllegalArgumentException("Entity must not be null");
+        }if (service == null){
+            throw new IllegalArgumentException("Service must not be null");
+        }
+        try {
+            entity = getFormData();
+            service.saveOrUpdate(entity);
+            Utils.currentStage(event).close();
+        }catch (DbException e){
+            Alerts.showAlert("Error saving object", null, e.getMessage(), Alert.AlertType.ERROR);
+        }
+    }
+
+    private Department getFormData() {
+        Department obj = new Department();
+        obj.setId(Utils.tryParseToInteger(txtId.getText()));
+        obj.setName(txtName.getText());
+        return obj;
+    }
+
+    @FXML
+    public void obBtCancelAction(ActionEvent event){
+        Utils.currentStage(event).close();
     }
 
     private void initializeNodes(){
